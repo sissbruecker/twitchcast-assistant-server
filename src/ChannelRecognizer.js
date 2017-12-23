539955ab88619a1553b7b82d8a736cacae9eaf11
Sascha Ißbrücker <sascha.issbruecker@byteagenten.de>
2017-12-23 16:21:07 +0100

    Initial commit

---
+++ src/ChannelRecognizer.js
@@ -0,0 +1,12 @@
+function ChannelMapper(mappings) {
+
+    function map(channel) {
+
+    }
+}
+
+function sanitize(channelInput) {
+    ''.tri
+}
+
+module.exports = ChannelMapper;
---
+++ src/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+let server;
+
+app.post('/stream/play/:channelId', async (req, res) => {
+
+    const channelId = req.params.channelId;
+
+    let streamInfo;
+    try {
+        streamInfo = await getSteamInfo(channelId, Quality.SINGLE_HD);
+    } catch (e) {
+        console.error(e);
+        res.status(500);
+        return res.json({
+            message: 'Could not load stream info',
+            error: e
+        });
+    }
+
+    try {
+        await player.play(streamInfo, Layout.CHAT_LEFT);
+    } catch (e) {
+        console.error(e);
+        res.status(500);
+        return res.json({
+            message: 'Could not start stream on Chromecast',
+            error: e
+        });
+    }
+
+    res.status(200);
+    res.json({
+        message: 'Playing channel',
+        channel: channelId
+    });
+});
+
+app.post('/stream/stop', async (req, res) => {
+
+    try {
+        await player.stop();
+    } catch (e) {
+        console.error(e);
+        res.status(500);
+        return res.json({
+            message: 'Could not stop app',
+            error: e
+        });
+    }
+
+    res.status(200);
+    res.json({
+        message: 'Stopped Twitchcast app'
+    });
+});
+
+function start(port) {
+    server = http.createServer(app);
+    server.listen(port);
+}
+
+module.exports = {
+    start
+};
